1a99837928ed6aa2d42c8e0f0a07695525bdf9c4
Chamath-0027 <chamatharthanayaka@gmail.com>
2025-07-02 02:08:23 +0530

    Add initial frontend setup with HTML, CSS, and main application structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/footer';
 import Home from './pages/Home';
-import Order from './pages/Order';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/order" element={<Order />} />
+
       </Routes>
+      <Footer />
+
     </Router>
   );
 }
