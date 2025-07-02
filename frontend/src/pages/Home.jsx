9872f109f10ffc963b7a8495d7e4b78b2a6819d6
Devmini wijerathna <chathudevmini@gmail.com>
2025-07-02 16:20:27 +0530

    add Navbar component and update Home page styling with Tailwind CSS

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Hotel Management System home page</h1>
+      <h1 class="text-3xl font-bold text-blue-800">Hotel Management System home page</h1>
    
      </div>
   );
